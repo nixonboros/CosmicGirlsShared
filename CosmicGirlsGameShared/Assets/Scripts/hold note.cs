514579ffbfd33794364342874118b8117b5cf296
nooodleees <166769236+nooodleees@users.noreply.github.com>
2024-04-19 20:50:34 +1000

    hold note object

---
+++ CosmicGirlsGameShared/Assets/Scripts/hold note.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class holdnote : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        meow meow
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
